f0cce0bf3d8e89493702d6b55490f94c8a13b96e
Nowa971 <ngitras@gmail>
2025-10-21 13:17:47 +0000

    a

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
- /**
- * main - Entry point
+
+/**
+ * main - Affiche une phrase avec printf
  *
- * Return: Always 0 (Success)
+ * Return: 0 si tout se passe bien
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
+
