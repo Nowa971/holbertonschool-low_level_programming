aeee882a36cf520439b29e51ba0ac1070253ac01
Nowa971 <ngitras@gmail.com>
2025-10-27 13:46:23 +0000

    0

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _isupper - checks for an uppercase character
+ * @c: character to check
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isupper(int c);
+
+#endif /* MAIN_H */
