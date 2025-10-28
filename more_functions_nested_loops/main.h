252a04f8645e4aa12a2242db794ec5a08771245a
Nowa971 <ngitras@gmail.com>
2025-10-28 09:56:47 +0000

    1

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -9,4 +9,13 @@
  */
 int _isupper(int c);
 
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: character to check
+ *
+ * Return: 1 if digit, 0 otherwise
+ */
+int _isdigit(int c);
+
 #endif /* MAIN_H */
+
